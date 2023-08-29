c1d53f69f36fee3d948513e657010d9521b4ece0
KHJ <tkdtkddmlehdanf97@gmail.com>
2023-08-29 19:53:50 +0900

    server test

--- test/Assets/KGHJ/Scripts/MoveCh.cs
+++ test/Assets/KGHJ/Scripts/MoveCh.cs
@@ -13,6 +13,17 @@ public class MoveCh : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("w 누르는주우우웅");
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("s 누르는주우우웅");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log("d 누르는주우우웅");
+        }
     }
 }
